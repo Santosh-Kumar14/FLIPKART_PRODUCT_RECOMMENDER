9b2881e91ae2434e2ffcc6d90b5333d932366ecd
Santosh-Kumar14 <kumar.santosh.diatm1@gmail.com>
2025-08-26 18:14:43 -0400

    flipkart recommender

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="FLIPKART-RECOMMENDER",
+    version="0.1",
+    author="Santosh",
+    packages=find_packages(),
+    install_requires = requirements,
+)
